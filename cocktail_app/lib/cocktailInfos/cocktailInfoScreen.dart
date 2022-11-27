ff1eadb82220609a3296bd647ca27692f356b001
Patajuade <Manon.Diana@icloud.com>
2022-11-27 21:38:55 +0100

    add working db connection

--- cocktail_app/lib/cocktailInfos/cocktailInfoScreen.dart
+++ cocktail_app/lib/cocktailInfos/cocktailInfoScreen.dart
@@ -1,45 +1,61 @@
+import 'dart:html';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cocktail_app/cocktailInfos/widgets/description.dart';
 import 'package:cocktail_app/cocktailInfos/widgets/header.dart';
 import 'package:cocktail_app/cocktailInfos/widgets/ingredients.dart';
 import 'package:cocktail_app/models/cocktail.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CocktailInfo extends StatelessWidget {
-  late Cocktail cocktail;
-
+  final FirebaseFirestore db= FirebaseFirestore.instance;
+  late Stream<QuerySnapshot> cocktailCollection;
+  
   CocktailInfo({super.key});
-
-  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
-    cocktail = const Cocktail(
-      description:
-          'The best cocktail everThe best cocktail everThe best cocktail everThe best cocktail everThe best cocktail everThe best cocktail everThe best cocktail everThe best cocktail ever',
-      name: 'Le Pouetjito',
-      id: '123456789',
-      ingredients: [
-        'Pouet',
-        'Vokda',
-        'Sirop de fruit de la passion',
-        'Schweppes'
-      ],
-      cocktailPicture:
-          'https://www.thecocktaildb.com/images/media/drink/2x8thr1504816928.jpg',
-      tags: ['#Alcool', '#Fruits', '#Passion', '#Vegan'],
-    );
-
+    cocktailCollection = db.collection("cocktails").snapshots();
     return Scaffold(
-      body: Column(
-        children: [
-          Header(cocktail: cocktail),
-          Description(
-            description: cocktail.description,
-            key: key,
-          ),
-          IngredientsList(cocktail: cocktail, key: key)
-        ],
-      ),
-    );
-  }
+      body:StreamBuilder<QuerySnapshot>(
+    stream:cocktailCollection,
+    builder:(context, snapshot) {
+      if (snapshot.hasError){
+        return const Text("Error");
+      }
+      if(snapshot.connectionState==ConnectionState.waiting){
+        return const Text("Loading");
+      }
+      return ListView(
+        children: snapshot.data!.docs.map(
+          (DocumentSnapshot document){
+            var data = document.data() as Map<String,dynamic>;
+            print(data);
+            return ListTile(
+              title: Text(data['description'])
+
+            );
+          }
+        ).toList(),
+      );
+    }
+  )
+  );
+
+  //   return Scaffold(
+  //     body: Column(
+  //       children: [
+  //         Header(cocktail: cocktail),
+  //         Description(
+  //           description: cocktail.description,
+  //           key: key,
+  //         ),
+  //         IngredientsList(cocktail: cocktail, key: key)
+  //       ],
+  //     ),
+  //   );
+  // }
+}
 }
--- cocktail_app/lib/main.dart
+++ cocktail_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cocktail_app/cocktailInfos/cocktailInfoScreen.dart';
 import 'package:cocktail_app/models/cocktail.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -6,16 +7,17 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'firebase_options.dart';
 
 void main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+ 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
