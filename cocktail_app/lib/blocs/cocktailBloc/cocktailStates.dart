627423ed1803a0d898801179598ea78511c855d1
Patajuade <manon.diana@icloud.com>
2022-12-15 12:30:11 +0100

    add comments on what to change in bloc

--- cocktail_app/lib/blocs/cocktailBloc/cocktailStates.dart
+++ cocktail_app/lib/blocs/cocktailBloc/cocktailStates.dart
@@ -16,5 +16,12 @@ class CocktailState {
     if (cocktail==null)throw Error();
     return Cocktail.fromJson(cocktail.data()!); 
   }
-
+  //aller mettre ça dans les blocs
+  //le fait de chercher la liste doit être dans le bloc
+  //le state doit être clean
+  // c'est pas ans state c'est dans bloc
+  // on<addCocktailEvent>((event, emit)=>
+  // cocktail = event.cocktail
+  // await FirebaseFirestore.instance.collection('cocktails').add(cocktail.toJson());
+  // emit(state([state.cocktails, cocktail]))
 }
